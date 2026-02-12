8e90a8f70b220f18d361719e7ac985a4ab5f34e1
Fahren Alfaqih <fahren21ti@mahasiswa.pcr.ac.id>
2026-02-12 13:38:55 +0700

    change data experiences

--- frontend/src/data/experiences.js
+++ frontend/src/data/experiences.js
@@ -6,7 +6,7 @@ export const experiences = [
     start_date: "2024-09-01T00:00:00Z",
     end_date: "2025-02-28T00:00:00Z",
     description:
-      "Contributed to the development of the Nursing Care Plan Form Module in the web-based Electronic Medical Records (EMR)...",
+      "Contributed to the development of the Nursing Care Plan Form Module in the web-based Electronic Medical Records (EMR) as part of the Hospital Management Information System (SIMRS). Supported hospital web server maintenance across three units (RS Bakti Timah Medika, RS Medika Stania, and RS Parittiga). Performed server monitoring, technical troubleshooting, and ensured system security, stability, and availability for internal users.",
   },
   {
     id: 1,
@@ -15,6 +15,6 @@ export const experiences = [
     start_date: "2024-02-08T00:00:00Z",
     end_date: "2024-06-15T00:00:00Z",
     description:
-      "Contributed to the development of a web-based Business Trip Recapitulation System to support the HCM Department...",
+      "Contributed to the development of a web-based Business Trip Recapitulation System to support the Human Capital Management (HCM) Department. Processed and recapitulated Official Travel Letter (SPD) data on a weekly, monthly, and annual basis. Provided information on total business trips and remaining budget per department to reduce manual calculations and streamline the reporting process.",
   },
 ];
